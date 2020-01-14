1f6b6a184ab6316c63a74523aaeae685ef778636
gvarin <varin.gaetan@gmail.com>
2020-01-14 14:11:30 +0100

    last commit before the next one

--- src/java/fr/solutec/dao/UserDao.java
+++ src/java/fr/solutec/dao/UserDao.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class UserDao {
     public static User getByLoginAndPass(String log, String mdp) throws SQLException{
         User u = null;
-        String sql = "SELECT * FROM user WHERE mail=? AND mdp=?";
+        String sql = "SELECT * FROM user WHERE login=? AND mdp=?";
         Connection connexion = AccessBD.getConnection();
         PreparedStatement requete = connexion.prepareStatement(sql);
         requete.setString(1, log);
@@ -25,18 +25,56 @@ public class UserDao {
             u.setId(rs.getInt("idUser"));
             u.setNom(rs.getString("nom"));
             u.setPrenom(rs.getString("prenom"));
-            u.setMail(rs.getString("mail"));
+            u.setLogin(rs.getString("login"));
         }
         return u;
     }
     
+    public static boolean isClient (int idUser) throws SQLException{
+        boolean res = false;
+        String sql = "SELECT idUser FROM client WHERE idUser=?";
+        Connection connexion = AccessBD.getConnection();
+        PreparedStatement requete = connexion.prepareStatement(sql);
+        requete.setInt(1, idUser);
+        ResultSet rs = requete.executeQuery();
+        if(rs.next()){
+            res=true;
+        }
+        return res;
+    }
+    
+    public static boolean isAdmin (int idUser) throws SQLException{
+        boolean res = false;
+        String sql = "SELECT idUser FROM admin WHERE idUser=?";
+        Connection connexion = AccessBD.getConnection();
+        PreparedStatement requete = connexion.prepareStatement(sql);
+        requete.setInt(1, idUser);
+        ResultSet rs = requete.executeQuery();
+        if(rs.next()){
+            res=true;
+        }
+        return res;
+    }
+    public static boolean isCons (int idUser) throws SQLException{
+        boolean res = false;
+        String sql = "SELECT idUser FROM conseiller WHERE idUser=?";
+        Connection connexion = AccessBD.getConnection();
+        PreparedStatement requete = connexion.prepareStatement(sql);
+        requete.setInt(1, idUser);
+        ResultSet rs = requete.executeQuery();
+        if(rs.next()){
+            res=true;
+        }
+        return res;
+    }
+    
     public static void inscription(User personne) throws SQLException{
-        String sql = "INSERT INTO user (nom, prenom, mail, mdp) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO user (nom, prenom, login, mdp) VALUES (?, ?, ?, ?)";
         Connection connexion = AccessBD.getConnection();
         PreparedStatement requete = connexion.prepareStatement(sql);
         requete.setString(1, personne.getNom());
         requete.setString(2, personne.getPrenom());
-        requete.setString(3, personne.getMail());
+        requete.setString(3, personne.getLogin());
         requete.setString(4, personne.getMdp());
         requete.execute();
         
@@ -55,7 +93,7 @@ public class UserDao {
             u.setId(rs.getInt("idUser"));
             u.setNom(rs.getString("nom"));
             u.setPrenom(rs.getString("prenom"));
-            u.setMail(rs.getString("mail"));
+            u.setLogin(rs.getString("login"));
             
             users.add(u);
         }
@@ -63,12 +101,12 @@ public class UserDao {
     }
     
    public static void modif(User personne) throws SQLException{
-        String sql = "UPDATE user SET nom=?, prenom=?, mail=? WHERE idUser=?";
+        String sql = "UPDATE user SET nom=?, prenom=?, login=? WHERE idUser=?";
         Connection connexion = AccessBD.getConnection();
         PreparedStatement requete = connexion.prepareStatement(sql);
         requete.setString(1, personne.getNom());
         requete.setString(2, personne.getPrenom());
-        requete.setString(3, personne.getMail());
+        requete.setString(3, personne.getLogin());
         requete.setInt(4, personne.getId());
         requete.executeUpdate
         ();
