887769a84619f7264df28ba6e45cc6017f2ca699
Ysydra <bruna.seferian@gmail.com>
2020-01-14 13:20:10 +0100

    BD

--- BD/BD.sql
+++ BD/BD.sql
@@ -1,228 +1,242 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 14 jan. 2020 à 12:17
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema banquedev18
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema banquedev18
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `banquedev18` DEFAULT CHARACTER SET utf8 ;
-USE `banquedev18` ;
-
--- -----------------------------------------------------
--- Table `banquedev18`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`user` (
-  `idUser` INT(11) NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NOT NULL,
-  `prenom` VARCHAR(45) NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `mdp` VARCHAR(45) NOT NULL,
-  `mail` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`idUser`),
-  UNIQUE INDEX `login_UNIQUE` (`login` ASC) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `banquedev18`.`admin`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`admin` (
-  `idAdmin` INT(11) NOT NULL AUTO_INCREMENT,
-  `idUser` INT(11) NOT NULL,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `banquedev18`
+--
+CREATE DATABASE IF NOT EXISTS `banquedev18` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `banquedev18`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `idAdmin` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
   PRIMARY KEY (`idAdmin`),
-  INDEX `fk_Admin_User_idx` (`idUser` ASC) ,
-  CONSTRAINT `fk_Admin_User`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `banquedev18`.`user` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_Admin_User_idx` (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `admin`
+--
 
--- -----------------------------------------------------
--- Table `banquedev18`.`conseiller`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`conseiller` (
-  `idConseiller` INT(11) NOT NULL AUTO_INCREMENT,
-  `photo` VARCHAR(450) NULL DEFAULT NULL,
-  `isActiv` TINYINT(4) NULL DEFAULT NULL,
-  `idUser` INT(11) NOT NULL,
-  PRIMARY KEY (`idConseiller`),
-  INDEX `fk_Conseiller_User1_idx` (`idUser` ASC) ,
-  CONSTRAINT `fk_Conseiller_User1`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `banquedev18`.`user` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
+INSERT INTO `admin` (`idAdmin`, `idUser`) VALUES
+(1, 1);
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `banquedev18`.`client`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`client` (
-  `idClient` INT(11) NOT NULL AUTO_INCREMENT,
-  `photo` VARCHAR(450) NULL DEFAULT NULL,
-  `dateNaissance` DATETIME NULL DEFAULT NULL,
-  `adresse` VARCHAR(405) NULL DEFAULT NULL,
-  `idUser` INT(11) NOT NULL,
-  `idConseiller` INT(11) NOT NULL,
-  PRIMARY KEY (`idClient`),
-  INDEX `fk_Client_User1_idx` (`idUser` ASC) ,
-  INDEX `fk_Client_Conseiller1_idx` (`idConseiller` ASC) ,
-  CONSTRAINT `fk_Client_Conseiller1`
-    FOREIGN KEY (`idConseiller`)
-    REFERENCES `banquedev18`.`conseiller` (`idConseiller`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Client_User1`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `banquedev18`.`user` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
+--
+-- Structure de la table `carte`
+--
 
-
--- -----------------------------------------------------
--- Table `banquedev18`.`compte`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`compte` (
-  `idCompte` INT(11) NOT NULL AUTO_INCREMENT,
-  `solde` INT(11) NULL DEFAULT NULL,
-  `decouvert` INT(11) NULL DEFAULT NULL,
-  `idClient` INT(11) NOT NULL,
-  `isActiv` TINYINT(4) NULL DEFAULT NULL,
-  PRIMARY KEY (`idCompte`),
-  INDEX `fk_Compte_Client1_idx` (`idClient` ASC) ,
-  CONSTRAINT `fk_Compte_Client1`
-    FOREIGN KEY (`idClient`)
-    REFERENCES `banquedev18`.`client` (`idClient`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `banquedev18`.`carte`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`carte` (
-  `idCarte` INT(11) NOT NULL AUTO_INCREMENT,
-  `isActiv` TINYINT(4) NULL DEFAULT NULL,
-  `idCompte` INT(11) NOT NULL,
+DROP TABLE IF EXISTS `carte`;
+CREATE TABLE IF NOT EXISTS `carte` (
+  `idCarte` int(11) NOT NULL AUTO_INCREMENT,
+  `isActiv` tinyint(4) DEFAULT NULL,
+  `idCompte` int(11) NOT NULL,
   PRIMARY KEY (`idCarte`),
-  INDEX `fk_Carte_Compte1_idx` (`idCompte` ASC) ,
-  CONSTRAINT `fk_Carte_Compte1`
-    FOREIGN KEY (`idCompte`)
-    REFERENCES `banquedev18`.`compte` (`idCompte`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_Carte_Compte1_idx` (`idCompte`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `carte`
+--
 
--- -----------------------------------------------------
--- Table `banquedev18`.`histocompte`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`histocompte` (
-  `idHistoCompte` INT(11) NOT NULL AUTO_INCREMENT,
-  `dateModifC` DATETIME NULL DEFAULT NULL,
-  `type` VARCHAR(45) NULL DEFAULT NULL,
-  `idCompte` INT(11) NOT NULL,
+INSERT INTO `carte` (`idCarte`, `isActiv`, `idCompte`) VALUES
+(1, 1, 1),
+(2, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `idClient` int(11) NOT NULL AUTO_INCREMENT,
+  `photo` varchar(450) DEFAULT NULL,
+  `dateNaissance` datetime DEFAULT NULL,
+  `adresse` varchar(405) DEFAULT NULL,
+  `idUser` int(11) NOT NULL,
+  `idConseiller` int(11) NOT NULL,
+  PRIMARY KEY (`idClient`),
+  KEY `fk_Client_User1_idx` (`idUser`),
+  KEY `fk_Client_Conseiller1_idx` (`idConseiller`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`idClient`, `photo`, `dateNaissance`, `adresse`, `idUser`, `idConseiller`) VALUES
+(1, 'https://vignette.wikia.nocookie.net/anime-characters-info/images/4/4e/Lord_Dio.jpg/revision/latest?cb=20190923201136', '1994-05-01 00:00:00', '1 rue du stand', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+DROP TABLE IF EXISTS `compte`;
+CREATE TABLE IF NOT EXISTS `compte` (
+  `idCompte` int(11) NOT NULL AUTO_INCREMENT,
+  `solde` int(11) DEFAULT NULL,
+  `decouvert` int(11) DEFAULT NULL,
+  `idClient` int(11) NOT NULL,
+  `isActiv` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`idCompte`),
+  KEY `fk_Compte_Client1_idx` (`idClient`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `compte`
+--
+
+INSERT INTO `compte` (`idCompte`, `solde`, `decouvert`, `idClient`, `isActiv`) VALUES
+(1, 5000, 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conseiller`
+--
+
+DROP TABLE IF EXISTS `conseiller`;
+CREATE TABLE IF NOT EXISTS `conseiller` (
+  `idConseiller` int(11) NOT NULL AUTO_INCREMENT,
+  `photo` varchar(450) DEFAULT NULL,
+  `isActiv` tinyint(4) DEFAULT NULL,
+  `idUser` int(11) NOT NULL,
+  PRIMARY KEY (`idConseiller`),
+  KEY `fk_Conseiller_User1_idx` (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `conseiller`
+--
+
+INSERT INTO `conseiller` (`idConseiller`, `photo`, `isActiv`, `idUser`) VALUES
+(1, 'https://www.exobaston.com/wp-content/uploads/2019/01/jotaro-jump.jpg', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `histocompte`
+--
+
+DROP TABLE IF EXISTS `histocompte`;
+CREATE TABLE IF NOT EXISTS `histocompte` (
+  `idHistoCompte` int(11) NOT NULL AUTO_INCREMENT,
+  `dateModifC` datetime DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `idCompte` int(11) NOT NULL,
   PRIMARY KEY (`idHistoCompte`),
-  INDEX `fk_HistoCompte_Compte1_idx` (`idCompte` ASC) ,
-  CONSTRAINT `fk_HistoCompte_Compte1`
-    FOREIGN KEY (`idCompte`)
-    REFERENCES `banquedev18`.`compte` (`idCompte`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_HistoCompte_Compte1_idx` (`idCompte`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `banquedev18`.`histoconnexion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`histoconnexion` (
-  `idHistoConnexion` INT(11) NOT NULL AUTO_INCREMENT,
-  `dateCo` VARCHAR(45) NOT NULL,
-  `idUser` INT(11) NOT NULL,
+--
+-- Structure de la table `histoconnexion`
+--
+
+DROP TABLE IF EXISTS `histoconnexion`;
+CREATE TABLE IF NOT EXISTS `histoconnexion` (
+  `idHistoConnexion` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCo` varchar(45) NOT NULL,
+  `idUser` int(11) NOT NULL,
   PRIMARY KEY (`idHistoConnexion`),
-  INDEX `fk_HistoConnexion_User1_idx` (`idUser` ASC) ,
-  CONSTRAINT `fk_HistoConnexion_User1`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `banquedev18`.`user` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_HistoConnexion_User1_idx` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `banquedev18`.`histocons`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`histocons` (
-  `idHistoCons` INT(11) NOT NULL AUTO_INCREMENT,
-  `dateModif` DATETIME NULL DEFAULT NULL,
-  `type` VARCHAR(45) NULL DEFAULT NULL,
-  `idConseiller` INT(11) NOT NULL,
+--
+-- Structure de la table `histocons`
+--
+
+DROP TABLE IF EXISTS `histocons`;
+CREATE TABLE IF NOT EXISTS `histocons` (
+  `idHistoCons` int(11) NOT NULL AUTO_INCREMENT,
+  `dateModif` datetime DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `idConseiller` int(11) NOT NULL,
   PRIMARY KEY (`idHistoCons`),
-  INDEX `fk_HistoCons_Conseiller1_idx` (`idConseiller` ASC) ,
-  CONSTRAINT `fk_HistoCons_Conseiller1`
-    FOREIGN KEY (`idConseiller`)
-    REFERENCES `banquedev18`.`conseiller` (`idConseiller`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_HistoCons_Conseiller1_idx` (`idConseiller`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `banquedev18`.`messages`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `banquedev18`.`messages` (
-  `idmessages` INT(11) NOT NULL AUTO_INCREMENT,
-  `message` VARCHAR(4500) NULL DEFAULT NULL,
-  `dateEnvoi` DATETIME NULL DEFAULT NULL,
-  `idClient` INT(11) NOT NULL,
-  `idConseiller` INT(11) NOT NULL,
+--
+-- Structure de la table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `idmessages` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(4500) DEFAULT NULL,
+  `dateEnvoi` datetime DEFAULT NULL,
+  `idClient` int(11) NOT NULL,
+  `idConseiller` int(11) NOT NULL,
   PRIMARY KEY (`idmessages`),
-  INDEX `fk_messages_Client1_idx` (`idClient` ASC) ,
-  INDEX `fk_messages_Conseiller1_idx` (`idConseiller` ASC) ,
-  CONSTRAINT `fk_messages_Client1`
-    FOREIGN KEY (`idClient`)
-    REFERENCES `banquedev18`.`client` (`idClient`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_Conseiller1`
-    FOREIGN KEY (`idConseiller`)
-    REFERENCES `banquedev18`.`conseiller` (`idConseiller`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_messages_Client1_idx` (`idClient`),
+  KEY `fk_messages_Conseiller1_idx` (`idConseiller`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `messages`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+INSERT INTO `messages` (`idmessages`, `message`, `dateEnvoi`, `idClient`, `idConseiller`) VALUES
+(1, 'Salut mon conseillé, c\'est du test', '2020-01-13 00:00:00', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `mdp` varchar(45) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `login_UNIQUE` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`idUser`, `nom`, `prenom`, `login`, `mdp`, `mail`) VALUES
+(1, 'admin1', 'aaa', 'admin1', 'admin1', 'admin1@mail.com'),
+(2, 'conseil1', 'ccc', 'cons1', 'cons1', 'cons1@mail.com'),
+(3, 'user1', 'uuu', 'user1', 'user1', 'user1@mail.com');
+
